09cb9f310a1c45be6649ca8cc6adf21c00acb743
Albert Ramos <seyn@outlook.com>
2023-01-20 15:29:15 +0100

    chore: create boilerplate

---
+++ test/index.spec.ts
@@ -0,0 +1,8 @@
+import Index from '@/routes/+page.svelte';
+import { render } from '@testing-library/svelte';
+
+test('Welcome the user', () => {
+	const { getByText } = render(Index);
+
+	expect(getByText('Title')).toBeInTheDocument();
+});
