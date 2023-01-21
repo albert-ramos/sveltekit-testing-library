bb6b385bf4162cacc3e730a411ddea0f91d38fb5
Albert Ramos <seyn@outlook.com>
2023-01-21 13:21:27 +0100

    fix: tests and mocks

---
+++ test/__mocks__/firebaseMock.js
@@ -0,0 +1,11 @@
+module.exports = {
+	getApps: jest.fn(() => []),
+	initializeApp: jest.fn(),
+	getApp: jest.fn(),
+	getFirestore: jest.fn(() => ({
+		collection: jest.fn()
+	})),
+	collection: jest.fn(() => ({
+		withConverter: jest.fn()
+	}))
+};
